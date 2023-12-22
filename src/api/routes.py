dab7c5bcf696f3f8b1c36f38c3552f317b8c3eba
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-12-22 20:20:20 +0000

    add login and signup

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,10 +3,14 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, get_hash
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+ 
 api = Blueprint('api', __name__)
+
 
 # Allow CORS requests to this API
 CORS(api)
@@ -20,3 +24,57 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    secure_password = get_hash(
+        password)
+    
+    new_user = User()
+    new_user.email = email
+    new_user.password = secure_password
+    new_user.is_active = True
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "User created"}), 201
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@api.route('/login', methods=['POST'])
+def login_user():
+  
+    email = request.json.get("email")
+    password = request.json.get("password")
+    
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "User not found"}), 401
+
+    if not get_hash(user.password, password):
+        return jsonify({"msg": "Wrong password"}), 401
+
+    token = create_access_token(
+        identity=user.id, additional_claims={"role": "admin"})
+    return jsonify({"msg": "Login succesful", "token": token}), 200
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@api.route("/get-hash", methods=["POST"])
+def handle_get_hash():
+    to_hash = request.json.get("string")
+    return get_hash(to_hash)
